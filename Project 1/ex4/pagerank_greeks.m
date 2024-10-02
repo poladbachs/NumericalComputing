e509cc32b4afedc3c6e07726abf02c9d5707a81f
Polad <bakhip@usi.ch>
2024-10-02 17:56:42 +0200

    ex4 disjoint subgraphs done

---
+++ Project 1/ex4/pagerank_greeks.m
@@ -0,0 +1,9 @@
+U = ["alpha", "beta", "gamma", "delta", "rho", "sigma"];
+i = [2, 3, 3, 4, 4, 1, 6, 5];
+j = [1, 1, 2, 2, 3, 4, 5, 6];
+n = 6;
+G = sparse(i, j, 1, n, n);
+full(G);
+
+p = 0.99;
+pagerank(U, G, p);
