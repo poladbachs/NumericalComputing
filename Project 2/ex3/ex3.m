07cb03c1837c5fe296a2bddaf1b96c40cd02bb9e
Polad <bakhip@usi.ch>
2024-10-23 01:54:28 +0200

    top 5 authors listed, their coauthors and degree centrality are displayed

--- Project 2/ex3/ex3.m
+++ Project 2/ex3/ex3.m
@@ -2,4 +2,20 @@ addpath('/Users/polad/NumericalComputing/Project 2/householder/');
 
 load('housegraph.mat');
 
-degree_centrality = sum(A, 2);
+full_name = cellstr(full(name));
+
+degree_centrality = full(sum(A, 2));
+
+[sorted_centrality, indices] = sort(degree_centrality, 'descend');
+top5_indices = indices(1:5);
+top5_centrality = sorted_centrality(1:5);
+
+fprintf('Top 5 authors based on degree centrality:\n');
+for i = 1:5
+    author = full_name{top5_indices(i)};
+    coauthors_indices = find(A(top5_indices(i), :) == 1);
+    coauthors = full_name(coauthors_indices);
+    
+    fprintf('%d. Author: %s, Degree Centrality: %d\n', i, author, full(top5_centrality(i)));
+    fprintf('   Coauthors: %s\n\n', strjoin(coauthors', ', '));
+end
